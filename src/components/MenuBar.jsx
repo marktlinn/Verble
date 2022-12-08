a62b8e43f46e725800ec792ffd97e734b92adabb
Mark Linn <marktlinn@gmail.com>
2022-12-08 23:12:48 +0100

    add: live links in slide-bar, needs styling

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -5,11 +5,15 @@ import { context } from "../hooks/Context";
 const MenuBar = () => {
   const { menuActive } = useContext(context);
   return (
-    <div className={menuActive ? "menu-slide-nav" : "menu-slide-closed"}>
+    <div
+      className={
+        menuActive ? "menu-slide-nav slide-bar" : "menu-slide-closed slide-bar"
+      }
+    >
       <h3>Other Games I've Created</h3>
 
-      <a href="#">game 1</a>
-      <a href="#">game 2</a>
+      <a href="https://qwizzical.netlify.app">Qwizzical</a>
+      <a href="https://tenzies-fe-app.netlify.app/">Tenzies</a>
       <div className="slide-footer">
         <Footer />
       </div>
