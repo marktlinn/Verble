c466c6ad99b05c3d120d2134b2bdc377960a3f81
Mark Linn <marktlinn@gmail.com>
2022-12-23 12:40:28 +0100

    refactor: updated styles, added icons, improved colouring

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import Footer from "./Footer";
 import { useContext } from "react";
 import { context } from "../hooks/Context";
+import {
+  faQuestion,
+  faGamepad,
+  faClock,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const MenuBar = () => {
   const { menuActive } = useContext(context);
   return (
@@ -10,10 +16,32 @@ const MenuBar = () => {
         menuActive ? "menu-slide-nav slide-bar" : "menu-slide-closed slide-bar"
       }
     >
-      <h3>Other Games I've Created</h3>
+      <div className="menuTitle">
+        <FontAwesomeIcon
+          className="menuIcon"
+          icon={faGamepad}
+          size="2x"
+        ></FontAwesomeIcon>
+        <h3>Other Games I've Created</h3>
+      </div>
 
-      <a href="https://qwizzical.netlify.app">Qwizzical</a>
-      <a href="https://tenzies-fe-app.netlify.app/">Tenzies</a>
+      <div className="menuItemBlock">
+        <div className="menuItem">
+          <FontAwesomeIcon
+            className="menuIcon"
+            icon={faQuestion}
+          ></FontAwesomeIcon>
+          <a href="https://qwizzical.netlify.app">Qwizzical</a>
+        </div>
+        <div className="menuItem">
+          <FontAwesomeIcon
+            className="menuIcon"
+            icon={faClock}
+          ></FontAwesomeIcon>
+          <a href="https://tenzies-fe-app.netlify.app/">Tenzies</a>
+        </div>
+      </div>
+
       <div className="slide-footer">
         <Footer />
       </div>
